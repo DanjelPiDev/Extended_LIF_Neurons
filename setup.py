6425948e2fd21e77117bf60defe8974cfb7e71c5
Dagait <danjelp.galic@gmail.com>
2025-02-07 19:15:53 +0100

    setup update

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="extended_lif_neurons",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "numpy",
+        "matplotlib",
+    ],
+    description="This repository presents an advanced LIF neuron model with dynamic spike probability, adaptive "
+                "thresholds, synaptic plasticity, and neuromodulation for more biologically realistic behavior. Key "
+                "features include membrane potential tracking, stochastic and deterministic spike generation, "
+                "adaptive excitability, and PyTorch integration.",
+    author="NullPointerExcy",
+    url="https://github.com/NullPointerExcy/Extended_LIF_Neurons",
+)
